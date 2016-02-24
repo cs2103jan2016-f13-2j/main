44a19381f3ca461cebfff8ba02a5c7dd3b7ed7ba
eeshuan <ngeeshuan@hotmail.com>
2016-02-24 11:53:02 +0800

    Implemented delete and edit methods

--- Trekker/src/Task.java
+++ Trekker/src/Task.java
@@ -11,12 +11,16 @@ public class Task implements Serializable {
 	//boolean alert;
 	String notes;
 	
+	//Constructor
 	public Task(String title, int startDate, int endDate, String notes) {
 		this.title = title;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.notes = notes;
 	}
+	
+	
+	//Get Functions
 	
 	public String getTitle() {
 		return title;
@@ -32,5 +36,24 @@ public class Task implements Serializable {
 	
 	public String getNotes() {
 		return notes;
+	}
+	
+	
+	//Edit Functions
+	
+	public void updateTitle(String text) {
+		title = text;
+	}
+	
+	public void updateStartDate(int date) {
+		startDate = date;
+	}
+	
+	public void updateEndDate(int date) {
+		endDate = date;
+	}
+	
+	public void updateNotes(String text) {
+		notes = text;
 	}
 }
--- Trekker/src/Trekker.java
+++ Trekker/src/Trekker.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -12,27 +10,42 @@ import java.util.*;
 public class Trekker {
 	
 	//-----Variables-----
+	
+	//File Variables
 	File file;
 	String fileName;
+	
+	//Running Program Variables
 	ArrayList<Task> taskList;
 	int numberOfTasks;
-	boolean run;
 	
+	//Global Variables
+	boolean run;
 	Scanner sc;
+	
 	
 	//MESSAGES
 		public final String MSG_COMMAND = "Enter Command: ";
 		public final String MSG_ADD = "Enter Task Details: <title> <startDate> <endDate> <notes>";
+		public final String MSG_DELETE = "Enter Task to Delete: <task number>";
+		public final String MSG_EDIT = "Enter Task to Edit: <task number>";
+		public final String MSG_EDIT_CHOICE = "Choose number to edit: <1. title> <2. startDate> <3. endDate> <4. notes>";
+		public final String MSG_EDIT_CHOICE_TO = "Edit to: ";
+		public final String MSG_NO_TASK = "No tasks to display.";
 		
 	//MESSAGES_SUCCESS
 		public final String MSG_ADD_SUCCESS = "Task added successfully.";
+		public final String MSG_DELETE_SUCCESS = "Task deleted successfully.";
+		public final String MSG_EDIT_SUCCESS = "Task updated successfully.";
 		
 	//MESSAGES_ERROR
 		public final String MSG_ERROR_COMMAND_NOT_FOUND = "Command not found.";
 		public final String MSG_ERROR_ADD_FAIL = "Failed to add task.";
+		public final String MSG_ERROR_DELETE_FAIL = "Failed to delete task.";
+		public final String MSG_ERROR_EDIT_FAIL = "Failed to edit task.";
 	
 	
-	//-----Basic Functions-----
+	//-----Basic Functions-----;
 	
 	private void addTask() {
 		try {
@@ -50,16 +63,43 @@ public class Trekker {
 	}
 	
 	private void deleteTask() {
-		
+		try {
+			showMessage(MSG_DELETE);
+			removeTask();
+			showMessage(MSG_DELETE_SUCCESS);
+		} catch (IndexOutOfBoundsException e) {
+			showMessage(MSG_ERROR_DELETE_FAIL);
+		}
 	}
 	
 	private void editTask() {
-		
+		try {
+			showMessage(MSG_EDIT);
+			int taskToEdit = Integer.parseInt(sc.nextLine());
+			Task task = taskList.get(taskToEdit-1);
+			showTask(task, taskToEdit);
+			showMessage(MSG_EDIT_CHOICE);
+			int lineToEdit = Integer.parseInt(sc.nextLine());
+			showMessage(MSG_EDIT_CHOICE_TO);
+			String newEdit = sc.nextLine();
+			task = updateTask(task, newEdit, lineToEdit);	
+			saveFile();
+			showMessage(MSG_EDIT_SUCCESS);
+		} catch (IndexOutOfBoundsException e) {
+			showMessage(MSG_ERROR_EDIT_FAIL);
+		} catch (NumberFormatException e) {
+			showMessage(MSG_ERROR_EDIT_FAIL);
+		}
 	}
 	
 	private void displayTask() {
-		for (int i=0; i<numberOfTasks; i++) {
-			showTask(taskList.get(i), i);
+		if (numberOfTasks == 0) {
+			showMessage(MSG_NO_TASK);
+		}
+		else {
+			for (int i=0; i<numberOfTasks; i++) {
+				showTask(taskList.get(i), i+1);
+			}
 		}
 	}
 	
@@ -105,7 +145,7 @@ public class Trekker {
 	}
 	
 	private void showTask(Task task, int taskNumber) {
-		showMessage("----- Task "+(taskNumber+1)+" -----");
+		showMessage("----- Task "+taskNumber+" -----");
 		showMessage("Task name: "+task.getTitle());
 		showMessage("Start Date: "+task.getStartDate());
 		showMessage("End Date: "+task.getEndDate());
@@ -130,6 +170,48 @@ public class Trekker {
 		int endDate = Integer.parseInt(sc.nextLine());
 		String notes = sc.nextLine();
 		return new Task(title, startDate, endDate, notes);
+	}
+	
+	private void removeTask() {
+		int taskToRemove = Integer.parseInt(sc.nextLine());
+		
+		taskList.remove(taskToRemove-1);
+		numberOfTasks--;
+		saveFile();
+	}
+	
+	private Task updateTask(Task task, String newEdit, int lineToEdit) {
+		boolean notValid = true;
+		
+		while (notValid) {
+			switch (lineToEdit) {
+			case 1: {			//title
+				task.updateTitle(newEdit);
+				notValid = false;
+				break;
+			}
+			case 2: {			//startDate
+				task.updateStartDate(Integer.parseInt(newEdit));
+				notValid = false;
+				break;
+			}
+			case 3: {			//endDate
+				task.updateEndDate(Integer.parseInt(newEdit));
+				notValid = false;
+				break;
+			}
+			case 4: {			//notes
+				task.updateNotes(newEdit);
+				notValid = false;
+				break;
+			}
+			default: {
+				notValid = true;
+			}
+			}
+		}
+		
+		return task;
 	}
 	
 	private void exitProgram() {
