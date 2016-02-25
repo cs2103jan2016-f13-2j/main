2771502de795e499ad446af2419f76285586b1a4
Bernard <a0124711u@u.nus.edu>
2016-02-25 12:49:44 +0800

    added changelog, made code quality changes

--- Trekker.java
+++ Trekker.java
@@ -8,45 +8,56 @@ import java.io.ObjectOutputStream;
 import java.util.*;
 
 public class Trekker {
-	
+
 	//-----Variables-----
-	
+
 	//File Variables
 	File file;
 	String fileName;
-	
+
 	//Running Program Variables
 	ArrayList<Task> taskList;
 	int numberOfTasks;
-	
+
 	//Global Variables
 	boolean run;
 	Scanner sc;
-	
-	
+
+
 	//MESSAGES
-		public final String MSG_COMMAND = "Enter Command: ";
-		public final String MSG_ADD = "Enter Task Details: <title> <startDate> <endDate> <notes>";
-		public final String MSG_DELETE = "Enter Task to Delete: <task number>";
-		public final String MSG_EDIT = "Enter Task to Edit: <task number>";
-		public final String MSG_EDIT_CHOICE = "Choose number to edit: <1. title> <2. startDate> <3. endDate> <4. notes>";
-		public final String MSG_EDIT_CHOICE_TO = "Edit to: ";
-		public final String MSG_NO_TASK = "No tasks to display.";
-		
+	public final String MSG_COMMAND = "Enter Command: ";
+	public final String MSG_ADD = "Enter Task Details: <title> <startDate> <endDate> <notes>";
+	public final String MSG_DELETE = "Enter Task to Delete: <task number>";
+	public final String MSG_EDIT = "Enter Task to Edit: <task number>";
+	public final String MSG_EDIT_CHOICE = "Choose number to edit: <1. title> <2. startDate> <3. endDate> <4. notes>";
+	public final String MSG_EDIT_CHOICE_TO = "Edit to: ";
+	public final String MSG_NO_TASK = "No tasks to display.";
+
 	//MESSAGES_SUCCESS
-		public final String MSG_ADD_SUCCESS = "Task added successfully.";
-		public final String MSG_DELETE_SUCCESS = "Task deleted successfully.";
-		public final String MSG_EDIT_SUCCESS = "Task updated successfully.";
-		
+	public final String MSG_ADD_SUCCESS = "Task added successfully.";
+	public final String MSG_DELETE_SUCCESS = "Task deleted successfully.";
+	public final String MSG_EDIT_SUCCESS = "Task updated successfully.";
+
 	//MESSAGES_ERROR
-		public final String MSG_ERROR_COMMAND_NOT_FOUND = "Command not found.";
-		public final String MSG_ERROR_ADD_FAIL = "Failed to add task.";
-		public final String MSG_ERROR_DELETE_FAIL = "Failed to delete task.";
-		public final String MSG_ERROR_EDIT_FAIL = "Failed to edit task.";
-	
-	
+	public final String MSG_ERROR_COMMAND_NOT_FOUND = "Command not found.";
+	public final String MSG_ERROR_ADD_FAIL = "Failed to add task.";
+	public final String MSG_ERROR_DELETE_FAIL = "Failed to delete task.";
+	public final String MSG_ERROR_EDIT_FAIL = "Failed to edit task.";
+
+
+	//-----Constructor-----
+
+	public Trekker() {
+		//Initialize variables
+		file = new File("task.txt");
+		fileName = "task.txt";
+		taskList = new ArrayList<Task>();
+
+		prepareFile();
+	}
+
 	//-----Basic Functions-----;
-	
+
 	private void addTask() {
 		try {
 			showMessage(MSG_ADD);
@@ -61,7 +72,7 @@ public class Trekker {
 			showMessage(MSG_ERROR_ADD_FAIL);
 		}
 	}
-	
+
 	private void deleteTask() {
 		try {
 			showMessage(MSG_DELETE);
@@ -71,7 +82,7 @@ public class Trekker {
 			showMessage(MSG_ERROR_DELETE_FAIL);
 		}
 	}
-	
+
 	private void editTask() {
 		try {
 			showMessage(MSG_EDIT);
@@ -91,7 +102,7 @@ public class Trekker {
 			showMessage(MSG_ERROR_EDIT_FAIL);
 		}
 	}
-	
+
 	private void displayTask() {
 		if (numberOfTasks == 0) {
 			showMessage(MSG_NO_TASK);
@@ -102,32 +113,32 @@ public class Trekker {
 			}
 		}
 	}
-	
-	
+
+
 	//-----Start-up Program Functions-----
-	
+
 	private void runCommandOptions() {
 		showMessage(MSG_COMMAND);
 		String command = sc.nextLine();
-		
-		switch (command.toUpperCase()) {
-		case "ADD": {
+
+		switch (command.toLowerCase()) {
+		case "add": {
 			addTask();
 			break;
 		}
-		case "DELETE": {
+		case "delete": {
 			deleteTask();
 			break;
 		}
-		case "EDIT": {
-			editTask();
-			break;
-		}
-		case "DISPLAY": {
+		case "display": {
 			displayTask();
 			break;
 		}
-		case "EXIT": {
+		case "edit": {
+			editTask();
+			break;
+		}
+		case "exit": {
 			exitProgram();
 			break;
 		}
@@ -137,13 +148,13 @@ public class Trekker {
 		}
 		}
 	}
-	
-	
+
+
 	//-----Helper Functions-----
-	private void showMessage(String message) {
-		System.out.println(message);
+	private void showMessage(String message, Object... args) {
+		System.out.printf(message, args).println();
 	}
-	
+
 	private void showTask(Task task, int taskNumber) {
 		showMessage("----- Task "+taskNumber+" -----");
 		showMessage("Task name: "+task.getTitle());
@@ -153,17 +164,17 @@ public class Trekker {
 		showMessage("");
 		showMessage("");
 	}
-	
+
 	private void prepareFile() {
 		if (file.exists()) {
 			readFile();
 		}
-		
+
 		else {
 			createFile();
 		}
 	}
-	
+
 	private Task createTask() {
 		String title = sc.nextLine();
 		int startDate = Integer.parseInt(sc.nextLine());
@@ -171,18 +182,18 @@ public class Trekker {
 		String notes = sc.nextLine();
 		return new Task(title, startDate, endDate, notes);
 	}
-	
+
 	private void removeTask() {
 		int taskToRemove = Integer.parseInt(sc.nextLine());
-		
+
 		taskList.remove(taskToRemove-1);
 		numberOfTasks--;
 		saveFile();
 	}
-	
+
 	private Task updateTask(Task task, String newEdit, int lineToEdit) {
 		boolean notValid = true;
-		
+
 		while (notValid) {
 			switch (lineToEdit) {
 			case 1: {			//title
@@ -210,28 +221,28 @@ public class Trekker {
 			}
 			}
 		}
-		
+
 		return task;
 	}
-	
+
 	private void exitProgram() {
 		saveFile();
 		run = false;
 	}
-	
-	
+
+
 	//-----File Readers-----
-	
+
 	//Reads the file and inputs it into the ArrayList
 	private void readFile() {
 		try {			
 			ObjectInputStream objectInputStream = new ObjectInputStream(
 					new FileInputStream("task.txt"));
-			
+
 			//Read number of Tasks
 			numberOfTasks = (Integer) objectInputStream.readObject();
-			
-			
+
+
 			Task task = null;
 			for (int i=0; i<numberOfTasks; i++) {
 				task = (Task) objectInputStream.readObject();
@@ -246,69 +257,56 @@ public class Trekker {
 		}
 
 	}
-	
+
 	//Saves the current testList to the text file
 	private void saveFile() {
 		try {
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(
-			        new FileOutputStream("task.txt"));
+					new FileOutputStream("task.txt"));
 			objectOutputStream.writeObject(numberOfTasks);
 			for (int i=0; i<numberOfTasks; i++) {
 				objectOutputStream.writeObject(taskList.get(i));
 			}
-			
+
 			objectOutputStream.close();
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	//Creates text file if it does not exist
 	private void createFile() {
 		try {
 			file.createNewFile();
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(
-	                new FileOutputStream("task.txt"));
+					new FileOutputStream("task.txt"));
 			objectOutputStream.writeObject(new Integer(0));
-			
+
 			objectOutputStream.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	//-----Constructor-----
-	
-	public Trekker() {
-		//Initialize variables
-		file = new File("task.txt");
-		fileName = "task.txt";
-		taskList = new ArrayList<Task>();
-		
-		prepareFile();
-	}
-	
-	private void run() {
+
+	private void runUntilExitCommand() {
 		//Show welcome message
-		
+
 		//Runs command options
 		run = true;
 		sc = new Scanner(System.in);
-		
+
 		while (run) {
 			runCommandOptions();
 		}
-		
+
 		sc.close();
 	}
-	
+
 	public static void main(String[] args) {
 		Trekker trekker = new Trekker();
-		trekker.run();
+		trekker.runUntilExitCommand();
 	}
 }
