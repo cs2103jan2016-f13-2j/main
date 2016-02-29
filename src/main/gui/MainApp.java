badbc703250f112d75b66862cae69dd67a9880c3
yangtse <geasnyt@hotmail.com>
2016-02-29 18:01:20 +0800

    first commit for YT

---
+++ src/main/gui/MainApp.java
@@ -0,0 +1,117 @@
+package main.gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import main.gui.model.Task;
+import main.gui.resources.TaskOverviewController;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+ // ... AFTER THE OTHER VARIABLES ...
+
+    /**
+     * The data as an observable list of Tasks.
+     */
+    private ObservableList<Task> taskData = FXCollections.observableArrayList();
+
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        taskData.add(new Task("wash hand", "Muster"));
+        taskData.add(new Task("wash leg", "Mueller"));
+        taskData.add(new Task("wash clothes", "Kurz"));
+        taskData.add(new Task("wash car", "Meier"));
+        taskData.add(new Task("wash nose", "Meyer"));
+        taskData.add(new Task("wash hair", "Kunz"));
+        taskData.add(new Task("wash shoe", "Best"));
+        taskData.add(new Task("wash toilet", "Meier"));
+        taskData.add(new Task("do work", "Mueller"));
+    }
+
+    /**
+     * Returns the data as an observable list of Tasks. 
+     * @return
+     */
+    public ObservableList<Task> getTaskData() {
+        return taskData;
+    }
+
+    // ... THE REST OF THE CLASS ...
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Trekker");
+
+        initRootLayout();
+
+        showTaskOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the task overview inside the root layout.
+     */
+    public void showTaskOverview() {
+        try {
+            // Load Task overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("TaskOverview.fxml"));
+            AnchorPane taskOverview = (AnchorPane) loader.load();
+
+            // Set task overview into the center of root layout.
+            rootLayout.setCenter(taskOverview);
+
+            // Give the controller access to the main app.
+            TaskOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
