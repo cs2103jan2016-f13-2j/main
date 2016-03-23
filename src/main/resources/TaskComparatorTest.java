081515970b01bd73a1cc3eb6982bb49e527f33de
Yang Tse <geasnyt@hotmail.com>
2016-03-23 23:13:41 +0800

    Changed Style of Floating Task table

---
+++ src/main/resources/TaskComparatorTest.java
@@ -0,0 +1,14 @@
+package main.resources;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskComparatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
